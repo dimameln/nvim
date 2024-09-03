58983dc86d11f08902e07464032bd7b4cf58ee4d
dimameln <dima_meln@bk.ru>
2024-09-03 22:27:21 +0300

    neovim config

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,27 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "golines",
+        "goimports",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function ()
+     return require "custom.configs.null-ls"
+    end,
+  },
+}
+return plugins
